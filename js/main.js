37905e86a394afc191a78f9aee6d4a4b3edf3634
Prince Ahmed <codingwithprince@gmail.com>
2021-08-04 22:44:55 +0600

    fixed deposit

--- js/main.js
+++ js/main.js
@@ -9,11 +9,20 @@ button.addEventListener('click',function(){
 // deposit event handler
 const depositButton = document.getElementById('deposit-btn');
  depositButton.addEventListener('click', function (){
+     let previousDeposit = document.getElementById("deposit").innerText;
+     let previousDepositToNumber = parseFloat(previousDeposit);
+    
      let depositInputAmount = document.getElementById('deposit-amount').value;
      let depositInputNumber = parseFloat(depositInputAmount);
-     document.getElementById('db').innerText = depositInputNumber;
+
+     let totalDeposit = previousDepositToNumber + depositInputNumber;
+     document.getElementById('deposit').innerText = totalDeposit;
+  
+// adding amount to Balance
      let balance = document.getElementById('balance').innerText;
      let balanceToNumber = parseFloat(balance);
      let totalBalance = balanceToNumber + depositInputNumber;
      document.getElementById('balance').innerText = totalBalance;
- })
+
+     document.getElementById('deposit-amount').value = "0";
+ });
