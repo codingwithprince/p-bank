caf8b6f8c42a1dbfa7732e5bdca4512cfd2edd20
Prince Ahmed <codingwithprince@gmail.com>
2021-08-04 22:14:33 +0600

    added events on click

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-// btn event listener
+// login event handler
 let button = document.querySelector('.btn');
 button.addEventListener('click',function(){
     document.getElementById('welcome-section').style.display = 'none';
     document.getElementById('main-area').style.display = 'block';
 });
+
+
+// deposit event handler
+const depositButton = document.getElementById('deposit-btn');
+ depositButton.addEventListener('click', function (){
+     let depositInputAmount = document.getElementById('deposit-amount').value;
+     let depositInputNumber = parseFloat(depositInputAmount);
+     console.log(depositInputNumber);
+  document.getElementById('db').innerText = depositInputNumber;
+
+ })
